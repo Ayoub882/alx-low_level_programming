47d96f59e9c76fb26eab7715f42410795db3b61c
Ayoub882 <ayoubrguig882@gmail.com>
2023-07-13 17:10:56 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - is the entry point function in a C program
+*return 0; :  is the statement that indicates successful program execution with an exit status of 0.
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}

e3796f32e41a5fd7b85010b86618c893f9f8071b
Ayoub882 <ayoubrguig882@gmail.com>
2023-07-13 17:24:33 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-/**
-*The main function  - is the entry point function in a C program
-*return : is the statement that indicates successful 
-*program execution with an exit status of 0.
-*/
-int main(void)
-{
+/*
+ * The main function is the entry point of the program.
+ * It returns an integer value to indicate the program's exit status.
+ * In this program, we return 0 to indicate successful execution.
+ */
+int main(void) {
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }

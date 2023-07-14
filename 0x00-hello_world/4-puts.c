0c2d9b18d0a014c099c3776b0167b556cda8f29a
Ayoub882 <ayoubrguig882@gmail.com>
2023-07-13 17:25:44 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,8 @@
  * It returns an integer value to indicate the program's exit status.
  * In this program, we return 0 to indicate successful execution.
  */
-int main(void) {
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }

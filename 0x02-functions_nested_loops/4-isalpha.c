124575d4c3aea1e2c676d797908b7fa971ed9425
Ayoub882 <ayoubrguig882@gmail.com>
2023-07-19 08:21:09 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha - Checks if a character is an alphabetic letter.
  * @c: The character to be checked.
  *
- * Return: 1 if c is an alphabetic letter 
+ * Return: 1 if c is an alphabetic letter
  * (lowercase or uppercase), 0 otherwise.
  */
 int _isalpha(int c)

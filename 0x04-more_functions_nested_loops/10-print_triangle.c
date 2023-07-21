0fe8e8889b5e71883cfb27d5cbbdb16d0353ee0f
Ayoub882 <ayoubrguig882@gmail.com>
2023-07-21 11:30:48 -0700

    F** triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,25 +6,25 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int x, y, z;
 
 	if (size <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
-
-	for (i = 1; i <= size; i++)
+	else
 	{
-		for (j = 1; j <= size - i; j++)
+		for (x = 0; x < size; x++)
 		{
-			_putchar(' ');
+			for (y = size - x; y > 1; y--)
+			{
+				_putchar(32);
+			}
+			for (z = 0; z <= x; z++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
 		}
-		for (j = 1; j <= i; j++)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
 	}
 }
-

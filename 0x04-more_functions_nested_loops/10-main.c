4fcaa78eb58a8dcf7a78cb3e0b97cbf612e7cb20
Ayoub882 <ayoubrguig882@gmail.com>
2023-07-21 11:09:34 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+int main(void)
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes a character to the standard output (stdout)
+ * @c: The character to print
+ *
+ * Return: On success, return the number of characters written.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
+

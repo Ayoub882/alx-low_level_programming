0d92b26427f1e27475bf84db690dfbd7a1cb2458
Ayoub882 <ayoubrguig882@gmail.com>
2023-07-21 18:08:01 -0700

    Numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,17 +6,23 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
+	 if (n == INT_MIN)
+	 {
+		 _putchar('-');
+		 _putchar('2');
+		 print_number(147483648);
+		 return;
+	 }
+	 if (n < 0)
+	 {
+		 _putchar('-');
+		 n = -n;
+	 }
+	 if (n / 10 != 0)
+	 {
+		 print_number(n / 10);
+	 }
 
-	if (n / 10 != 0)
-	{
-		print_number(n / 10);
-	}
-
-	_putchar((n % 10) + '0');
+	 _putchar((n % 10) + '0');
 }
 

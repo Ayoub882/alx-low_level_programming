803cd538de2fdef6f17dd0c3438606dceb231ec4
Ayoub882 <ayoubrguig882@gmail.com>
2023-07-27 16:48:56 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
 * infinite_add - Adds two numbers.
@@ -7,38 +8,38 @@
 * @r: Pointer to the buffer to store the result.
 * @size_r: Size of the buffer @r.
 *
-* Return: Pointer to the result (stored in @r) or 0 if the result cannot fit.
+* Return: Pointer to the result (stored in a dynamically allocated memory)
+*or NULL if the result cannot fit.
 */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int i, j, k, len1, len2, sum, carry;
-
-len1 = 0;
+int len1 = 0, len2 = 0;
 while (n1[len1] != '\0')
 len1++;
-
-len2 = 0;
 while (n2[len2] != '\0')
 len2++;
 
-if (size_r <= len1 || size_r <= len2)
-return 0;
+if (size_r <= len1 || size_r <= len2 || size_r == 0)
+return (NULL);
 
 r[size_r - 1] = '\0';
-i = len1 - 1;
-j = len2 - 1;
-k = size_r - 2;
-carry = 0;
+int i = len1 - 1;
+int j = len2 - 1;
+int k = size_r - 2;
+int carry = 0;
 
 while (i >= 0 || j >= 0 || carry > 0)
 {
-sum = carry;
+int sum = carry;
 if (i >= 0)
 sum += n1[i] - '0';
 if (j >= 0)
 sum += n2[j] - '0';
 
 carry = sum / 10;
+if (k < 0)
+return (NULL);
+
 r[k] = (sum % 10) + '0';
 
 i--;
@@ -46,5 +47,6 @@ j--;
 k--;
 }
 
-return &r[k + 1];
+return (&r[k + 1]);
 }
+

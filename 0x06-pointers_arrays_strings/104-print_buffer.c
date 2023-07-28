d7e1383474100502b407bbbe70a0f93b9143ca6b
Ayoub882 <ayoubrguig882@gmail.com>
2023-07-28 13:15:01 -0700

    Noise is a buffer, more effective than cubicles or booth walls

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,52 +1,51 @@
-#include <stdio.h>
-#include <ctype.h>
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
+ */
 
 void print_buffer(char *b, int size)
 {
-int i;
-int j;
-if (size <= 0)
-{
-printf("\n");
-return;
+	int o, j, i;
+
+	o = 0;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (o < size)
+	{
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+	}
 }
-for (i = 0; i < size; i += 10)
-{
-printf("%08x: ", i);
-for (j = i; j < i + 10; j++)
-{
-if (j < size)
-{
-printf("%02x", (unsigned char)b[j]);
-}
-else
-{
-printf("  ");
-}
-if (j % 2 != 0)
-{
-printf(" ");
-}
-}
-for (j = i; j < i + 10; j++)
-{
-if (j < size)
-{
-if (isprint(b[j]))
-{
-printf("%c", b[j]);
-}
-else
-{
-printf(".");
-}
-}
-else
-{
-printf(" ");
-}
-}
-printf("\n");
-}
-}
+

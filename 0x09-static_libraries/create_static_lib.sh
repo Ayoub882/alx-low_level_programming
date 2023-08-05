06be3b3dca32fc38954ad01c65b6f8109f18d7fe
Ayoub882 <ayoubrguig882@gmail.com>
2023-08-04 19:48:15 -0700

    Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
+
+# Compile all .c files into object files, except test_functions.c
+gcc -Wall -pedantic -Werror -Wextra -c $(ls *.c | grep -v "test_functions.c")
+
+# Create the static library liball.a
 ar -rc liball.a *.o
+
+# Update the table of contents in the library
 ranlib liball.a
+
+# Optionally, you can also remove the test_functions executable if it's present
+# rm -f test_functions
 

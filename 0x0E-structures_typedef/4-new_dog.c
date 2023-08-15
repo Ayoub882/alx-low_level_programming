a66a6d39d79e16f09030f1008c8ff5857ea81897
Ayoub882 <ayoubrguig882@gmail.com>
2023-08-14 17:07:25 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include "dog.h"
 #include <stdlib.h>
 #include <string.h>
+#include "dog.h"
 
 /**
  * new_dog - Creates a new dog
@@ -13,34 +13,34 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog_ptr;
-    char *name_copy, *owner_copy;
+	dog_t *new_dog_ptr;
+	char *name_copy, *owner_copy;
 
-    /* Allocate memory for the new dog structure */
-    new_dog_ptr = malloc(sizeof(dog_t));
-    if (new_dog_ptr == NULL)
-        return (NULL);
+	/* Allocate memory for the new dog structure */
+	new_dog_ptr = malloc(sizeof(dog_t));
+	if (new_dog_ptr == NULL)
+		return (NULL);
 
-    /* Allocate memory for name and owner copies */
-    name_copy = malloc(sizeof(char) * (strlen(name) + 1));
-    owner_copy = malloc(sizeof(char) * (strlen(owner) + 1));
-    if (name_copy == NULL || owner_copy == NULL)
-    {
-        free(new_dog_ptr);
-        free(name_copy);
-        free(owner_copy);
-        return (NULL);
-    }
+	/* Allocate memory for name and owner copies */
+	name_copy = malloc(sizeof(char) * (strlen(name) + 1));
+	owner_copy = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (name_copy == NULL || owner_copy == NULL)
+	{
+		free(new_dog_ptr);
+		free(name_copy);
+		free(owner_copy);
+		return (NULL);
+	}
 
-    /* Copy name and owner strings */
-    strcpy(name_copy, name);
-    strcpy(owner_copy, owner);
+	/* Copy name and owner strings */
+	strcpy(name_copy, name);
+	strcpy(owner_copy, owner);
 
-    /* Assign values to the new dog structure */
-    new_dog_ptr->name = name_copy;
-    new_dog_ptr->age = age;
-    new_dog_ptr->owner = owner_copy;
+	/* Assign values to the new dog structure */
+	new_dog_ptr->name = name_copy;
+	new_dog_ptr->age = age;
+	new_dog_ptr->owner = owner_copy;
 
-    return (new_dog_ptr);
+	return (new_dog_ptr);
 }
 

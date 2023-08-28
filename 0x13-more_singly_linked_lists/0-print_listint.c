6b8094f7099de559c0566fcf4bb8cd2d66aed063
Ayoub882 <ayoubrguig882@gmail.com>
2023-08-28 10:54:50 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,18 +9,17 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t node_count = 0;
+    size_t node_count = 0;
 
-	while (h != NULL)
-	{
-		/* Use _putchar to print the value as a character */
-		char digit = h->n + '0'; /*Convert integer to character*/
-		_putchar(digit); /*Call _putchar to print the character*/
-		_putchar('\n'); /*Print newline character*/
-		h = h->next;
-		node_count++;
-	}
+    while (h != NULL)
+    {
+        char digit = h->n + '0'; /*Convert integer to character*/
+        _putchar(digit); /*Call _putchar to print the character*/
+        _putchar('\n'); /*Print newline character*/
+        h = h->next;
+        node_count++;
+    }
 
-	return (node_count);
+    return (node_count);
 }
 
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -13,9 +13,9 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
-int _putchar(char c);
 
 size_t print_listint(const listint_t *h);
+int _putchar(char c);
 
 #endif /* LISTS_H */
 

ca38d0e7151eadc7468967a46477d551aad516fb
Ayoub882 <ayoubrguig882@gmail.com>
2023-08-31 08:55:46 -0700

    1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,10 +3,13 @@
 
 #include <stddef.h>
 
-/* Function to convert binary to unsigned int */
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
-/* Custom _putchar function */
 int _putchar(char c);
 
 #endif /* MAIN_H */

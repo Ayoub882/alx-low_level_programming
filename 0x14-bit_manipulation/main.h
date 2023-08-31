3413455b2f890682ec09f6cda803e636e7edd343
Ayoub882 <ayoubrguig882@gmail.com>
2023-08-31 08:48:56 -0700

    0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,13 +3,6 @@
 
 #include <stddef.h>
 
-/* Structure for singly linked list */
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *next;
-} listint_t;
-
 /* Function to convert binary to unsigned int */
 unsigned int binary_to_uint(const char *b);
 

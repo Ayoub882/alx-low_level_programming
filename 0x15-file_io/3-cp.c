2e0d804ac2b862ffe96435ccee87be6e2d1b4924
Ayoub882 <ayoubrguig882@gmail.com>
2023-09-05 03:18:46 +0100

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,80 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 1024
 
 /**
- * display_error - Display an error message
- * and exit with the given status code.
- * @message: The error message to display.
- * @status: The status code to exit with.
+ * error_exit - Exit the program with an error code and print an error message.
+ * @code: The error code to exit with.
+ * @message: The error message to print.
  */
-void display_error(const char *message, int status)
+void error_exit(int code, const char *message)
 {
 	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(status);
+	exit(code);
 }
+/**
+ * open_source_file - Open the source file and handle errors.
+ * @file_from: The source file path.
+ * Return: The file descriptor of the source file.
+ */
+int open_source_file(const char *file_from)
+{
+	int fd_from = open(file_from, O_RDONLY);
 
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		error_exit(98, "Error: Can't read from source file");
+	}
+	return (fd_from);
+}
+/**
+ * open_destination_file - Open the destination file and handle errors.
+ * @file_to: The destination file path.
+ * Return: The file descriptor of the destination file.
+ */
+int open_destination_file(const char *file_to)
+{
+	int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+		error_exit(99, "Error: Can't write to destination file");
+	}
+	return (fd_to);
+}
 /**
  * copy_file - Copy the content of one file to another.
- * @source: The source file descriptor.
- * @dest: The destination file descriptor.
+ * @fd_from: The file descriptor of the source file.
+ * @fd_to: The file descriptor of the destination file.
  */
-void copy_file(int source, int dest)
+void copy_file(int fd_from, int fd_to)
 {
-	char buffer[1024];
+	char buffer[BUFFER_SIZE];
 	ssize_t bytes_read, bytes_written;
 
-	while ((bytes_read = read(source, buffer, sizeof(buffer))) > 0)
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		bytes_written = write(dest, buffer, bytes_read);
-		if (bytes_written != bytes_read)
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
 		{
-			display_error("Error: Can't write to destination file", 99);
+			dprintf(STDERR_FILENO, "Error: Can't write to destination file\n");
+			error_exit(99, "Error: Can't write to destination file");
 		}
 	}
 
 	if (bytes_read == -1)
 	{
-		display_error("Error: Can't read from source file", 98);
+		dprintf(STDERR_FILENO, "Error: Can't read from source file\n");
+		error_exit(98, "Error: Can't read from source file");
 	}
 }
-
 /**
- * main - Copies the content of one file to another.
+ * main - Entry point of the program.
  * @argc: The number of command-line arguments.
- * @argv: An array of command-line argument strings.
- *
- * Return: 0 on success, or an error code on failure.
+ * @argv: An array of command-line arguments.
+ * Return: 0 on success, exit with appropriate error codes on failure.
  */
 int main(int argc, char *argv[])
 {
+	const char *file_from, *file_to;
 	int fd_from, fd_to;
 
 	if (argc != 3)
 	{
-		display_error("Usage: cp file_from file_to", 97);
+		error_exit(97, "Usage: cp file_from file_to");
 	}
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		display_error("Error: Can't read from source file", 98);
-	}
+	file_from = argv[1];
+	file_to = argv[2];
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		display_error("Error: Can't write to destination file", 99);
-	}
+	fd_from = open_source_file(file_from);
+	fd_to = open_destination_file(file_to);
 
 	copy_file(fd_from, fd_to);
 
 	if (close(fd_from) == -1 || close(fd_to) == -1)
 	{
-		display_error("Error: Can't close file descriptor", 100);
+		dprintf(STDERR_FILENO, "Error: Can't close file descriptor\n");
+		error_exit(100, "Error: Can't close file descriptor");
 	}
 
 	return (0);

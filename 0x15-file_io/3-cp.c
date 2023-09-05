165b7dbdfd0b8014ed64ae642cda5478f86c2e73
Ayoub882 <ayoubrguig882@gmail.com>
2023-09-05 03:22:36 +0100

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,107 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <errno.h>
-
-#define BUFFER_SIZE 1024
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 /**
- * error_exit - Exit the program with an error code and print an error message.
- * @code: The error code to exit with.
- * @message: The error message to print.
+ * main - Copies the content of one file to another
+ * @argc: The number of arguments
+ * @argv: An array of arguments
+ *
+ * Return: 0 on success, or an error code on failure
  */
-void error_exit(int code, const char *message)
+int main(int argc, char *argv[])
 {
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(code);
-}
-/**
- * open_source_file - Open the source file and handle errors.
- * @file_from: The source file path.
- * Return: The file descriptor of the source file.
- */
-int open_source_file(const char *file_from)
-{
-	int fd_from = open(file_from, O_RDONLY);
+	int fd_from, fd_to, bytes_read, bytes_written;
+	char buffer[1024];
 
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		error_exit(98, "Error: Can't read from source file");
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-	return (fd_from);
-}
-/**
- * open_destination_file - Open the destination file and handle errors.
- * @file_to: The destination file path.
- * Return: The file descriptor of the destination file.
- */
-int open_destination_file(const char *file_to)
-{
-	int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
-		error_exit(99, "Error: Can't write to destination file");
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fd_from);
+		exit(99);
 	}
-	return (fd_to);
-}
-/**
- * copy_file - Copy the content of one file to another.
- * @fd_from: The file descriptor of the source file.
- * @fd_to: The file descriptor of the destination file.
- */
-void copy_file(int fd_from, int fd_to)
-{
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read, bytes_written;
 
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
+		if (bytes_written != bytes_read)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to destination file\n");
-			error_exit(99, "Error: Can't write to destination file");
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
 		}
 	}
 
 	if (bytes_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from source file\n");
-		error_exit(98, "Error: Can't read from source file");
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
 	}
-}
-/**
- * main - Entry point of the program.
- * @argc: The number of command-line arguments.
- * @argv: An array of command-line arguments.
- * Return: 0 on success, exit with appropriate error codes on failure.
- */
-int main(int argc, char *argv[])
-{
-	const char *file_from, *file_to;
-	int fd_from, fd_to;
-
-	if (argc != 3)
-	{
-		error_exit(97, "Usage: cp file_from file_to");
-	}
-
-	file_from = argv[1];
-	file_to = argv[2];
-
-	fd_from = open_source_file(file_from);
-	fd_to = open_destination_file(file_to);
-
-	copy_file(fd_from, fd_to);
 
 	if (close(fd_from) == -1 || close(fd_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptor\n");
-		error_exit(100, "Error: Can't close file descriptor");
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (close(fd_from) == -1) ? fd_from : fd_to);
+		exit(100);
 	}
 
 	return (0);

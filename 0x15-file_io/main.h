9d791166ad3e5163cf322f9ef721ed056ed8952a
Ayoub882 <ayoubrguig882@gmail.com>
2023-09-05 03:40:00 +0100

    cp

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define BUFFER_SIZE 1024
+
 #include <stddef.h>
 #include <unistd.h>
 
@@ -11,6 +13,12 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _strlen(char *str);
 void close_file(int fd);
+
+/*New functions prototypes*/
+int open_source_file(const char *file_name);
+int open_destination_file(const char *file_name);
+void copy_contents(int fd_from, int fd_to);
+void close_files(int fd_from, int fd_to);
 
 #endif /* MAIN_H */
 

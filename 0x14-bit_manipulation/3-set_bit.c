e5127c4816c643601d9a290ededa7dbd0c308ddb
Ayoub882 <ayoubrguig882@gmail.com>
2023-09-29 02:20:38 +0100

    3. 11

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -15,8 +15,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1); /**/
 
-	unsigned long int mask = 1UL << index;
-
 	*n = *n | mask; /*Set the bit to 1*/
 
 	return (1); /*Return 1 to indicate success*/

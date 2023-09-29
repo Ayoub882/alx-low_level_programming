aaa6a73d8cf8374bc4cac9a8e56f387bfb438d9f
Ayoub882 <ayoubrguig882@gmail.com>
2023-09-29 02:55:12 +0100

    get_endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -4,6 +4,8 @@
  * get_endianness - Checks the endianness of the system.
  *
  * Return: 0 if big endian, 1 if little endian.
+ * determine the endianness of a system by
+ * checking the value of a multi-byte integer.
  */
 int get_endianness(void)
 {

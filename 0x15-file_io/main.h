b38029d6809d8b2cef59a034c250b20d76851034
Ayoub882 <ayoubrguig882@gmail.com>
2023-10-03 00:58:22 +0100

    cp

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,6 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+int copy_file(int src_fd, int dest_fd, const char *filename_from, const char *filename_to);
 
 int open_source_file(const char *file_name);
 int open_destination_file(const char *file_name);

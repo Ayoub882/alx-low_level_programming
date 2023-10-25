f12e7329d7a7363f96f3c125ddcadbbd4d132ffb
Ayoub882 <ayoubrguig882@gmail.com>
2023-10-25 23:28:49 +0100

    new_25_10

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,47 +1,43 @@
 #include <stdio.h>
-#include <stddef.h>
 #include <stdlib.h>
 #include "lists.h"
+#include <unistd.h>
 
 /**
- * print_listint_safe - Prints a listint_t linked list safely.
- * @head: A pointer to the head of the linked list.
+ * print_listint_safe - Prints a listint_t linked list
+ * @head: Pointer to the head of the linked list
  *
- * Return: The number of nodes in the list.
+ * Return: The number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow = head, *fast = head;
-	size_t count = 0, loop_detected = 0;
+	size_t count = 0;
 
-	while (slow != NULL && fast != NULL && fast->next != NULL)
+	while (head != NULL)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
+		char buffer[20];
+		int len = snprintf(buffer, sizeof(buffer), "%d");
+		write(STDOUT_FILENO, "[", 1);
+		write(STDOUT_FILENO, (void *)head, sizeof(void *));
+		write(STDOUT_FILENO, "] ", 2);
+		write(STDOUT_FILENO, buffer, len);
+		write(STDOUT_FILENO, "\n", 1);
 		count++;
 
-		printf("[%p] %d\n", (void *)slow, slow->n);
-
-		if (slow == fast)
+		if (head <= head->next)
 		{
-			loop_detected = 1;
-			break; /* Loop detected, break the loop */
+			write(STDERR_FILENO, "-> [", 4);
+			write(STDERR_FILENO, (void *)head->next, sizeof(void *));
+			write(STDERR_FILENO, "] ", 2);
+			len = snprintf(buffer, sizeof(buffer), "%d", head->next->n);
+			write(STDERR_FILENO, buffer, len);
+			write(STDERR_FILENO, "\n", 1);
+			exit(98);
 		}
+
+		head = head->next;
 	}
 
-	if (loop_detected)
-	{
-		slow = head;
-		while (slow != fast)
-		{
-			printf("[%p] %d\n", (void *)slow, slow->n);
-			slow = slow->next;
-			fast = fast->next;
-		}
-		printf("-> [%p] %d\n", (void *)slow, slow->n);
-		exit(98); /* List contains a loop, exit with status 98 */
-	}
-
-	return (count);
+	return count;
 }
 

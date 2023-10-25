ffcf2488ea27ba58ed718b34efda2011c7ae7bee
Ayoub882 <ayoubrguig882@gmail.com>
2023-10-26 00:04:15 +0100

    new_25_10

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,20 +12,35 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *current = head;
+	const listint_t *tortoise = head, *hare = head;
 
-	while (current != NULL)
+	char buffer[20];
+	int len;
+
+	while (hare != NULL && hare->next != NULL)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+
+		len = snprintf(buffer, sizeof(buffer), "%d", tortoise->n);
+
+		write(STDOUT_FILENO, "[", 1);
+		write(STDOUT_FILENO, (void *)&tortoise, sizeof(void *));
+		write(STDOUT_FILENO, "] ", 2);
+		write(STDOUT_FILENO, buffer, len);
+		write(STDOUT_FILENO, "\n", 1);
 		count++;
 
-		if (current <= current->next)
+		if (tortoise == hare)
 		{
-			fprintf(stderr, "-> [%p] %d\n", (void *)current->next, current->next->n);
+			len = snprintf(buffer, sizeof(buffer), "%d", hare->n);
+			write(STDOUT_FILENO, "-> [", 4);
+			write(STDOUT_FILENO, (void *)&hare, sizeof(void *));
+			write(STDOUT_FILENO, "] ", 2);
+			write(STDOUT_FILENO, buffer, len);
+			write(STDOUT_FILENO, "\n", 1);
 			break;
 		}
-
-		current = current->next;
 	}
 
 	return (count);

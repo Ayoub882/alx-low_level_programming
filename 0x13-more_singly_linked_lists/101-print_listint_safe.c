6ce2bd1abecb0ee359b25e0db72f344aedf971bf
Ayoub882 <ayoubrguig882@gmail.com>
2023-10-26 00:08:52 +0100

    new_25_10

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -4,18 +4,21 @@
 #include <unistd.h>
 
 /**
- * print_listint_safe - Prints a listint_t linked list
- * @head: Pointer to the head of the linked list
+ * print_listint_safe - Prints a listint_t linked list.
+ * @head: Pointer to the head of the linked list.
  *
- * Return: The number of nodes in the list
+ * Return: The number of nodes in the list.
+ * This function can print lists with a loop.
+ * You should go through the list only once.
+ * If the function fails, exit the program with status 98.
+ * Output format: [address] n
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
 	const listint_t *tortoise = head, *hare = head;
-
-	char buffer[20];
 	int len;
+	char buffer[20];
 
 	while (hare != NULL && hare->next != NULL)
 	{
@@ -39,7 +42,7 @@ size_t print_listint_safe(const listint_t *head)
 			write(STDOUT_FILENO, "] ", 2);
 			write(STDOUT_FILENO, buffer, len);
 			write(STDOUT_FILENO, "\n", 1);
-			break;
+			exit(98);
 		}
 	}
 

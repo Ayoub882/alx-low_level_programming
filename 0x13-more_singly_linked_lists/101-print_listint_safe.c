50c7258740c61d79b5022788222e9ae63faabd09
Ayoub882 <ayoubrguig882@gmail.com>
2023-10-26 00:55:57 +0100

    new_25_10

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -42,8 +42,8 @@ size_t print_listint_safe(const listint_t *head)
 			write(STDOUT_FILENO, "] ", 2);
 			write(STDOUT_FILENO, buffer, len);
 			write(STDOUT_FILENO, "\n", 1);
-			exit(98);
 			break;
+			exit(98);
 		}
 	}
 

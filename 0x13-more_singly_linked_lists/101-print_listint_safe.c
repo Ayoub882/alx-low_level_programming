6026c3c7a434c038b9d4d1b46708d92c0aacc26a
Ayoub882 <ayoubrguig882@gmail.com>
2023-10-26 00:49:22 +0100

    new_25_10

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include "lists.h"
+#include <unistd.h>
 
 /**
  * print_listint_safe - Prints a listint_t linked list.
@@ -16,21 +16,35 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *current = head;
-	const listint_t *loop_node = NULL;
+	const listint_t *tortoise = head, *hare = head;
+	char buffer[20];
+	int len;
 
-	while (current)
+	while (hare != NULL && hare->next != NULL)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+
+		len = snprintf(buffer, sizeof(buffer), "%d", tortoise->n);
+
+		write(STDOUT_FILENO, "[", 1);
+		write(STDOUT_FILENO, (void *)&tortoise, sizeof(void *));
+		write(STDOUT_FILENO, "] ", 2);
+		write(STDOUT_FILENO, buffer, len);
+		write(STDOUT_FILENO, "\n", 1);
 		count++;
 
-		if (current <= loop_node)
+		if (tortoise == hare)
 		{
+			len = snprintf(buffer, sizeof(buffer), "%d", hare->n);
+			write(STDOUT_FILENO, "-> [", 4);
+			write(STDOUT_FILENO, (void *)&hare, sizeof(void *));
+			write(STDOUT_FILENO, "] ", 2);
+			write(STDOUT_FILENO, buffer, len);
+			write(STDOUT_FILENO, "\n", 1);
 			exit(98);
+			break;
 		}
-
-		loop_node = current;
-		current = current->next;
 	}
 
 	return (count);
